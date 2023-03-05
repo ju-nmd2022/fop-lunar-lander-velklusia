5b5d944c156826d44d45338c7a945b17c0c06e0f
velklusia <116886960+velklusia@users.noreply.github.com>
2023-03-06 00:46:24 +0100

    Asteroid ()

--- startscreen.js
+++ startscreen.js
@@ -26,3 +26,38 @@ function draw() {
     asteroids.push(new Asteroid(random(width), random(height)));
   }
 }
+class Asteroid {
+  constructor(x, y) {
+    this.position = createVector(x, y);
+    this.prevPosition = createVector(x, y);
+    
+    this.velocity = createVector(0, 0);
+    
+    this.ang = atan2(y - (height/2), x - (width/5));
+  }
+  
+  isActive() {
+    return onScreen(this.prevPosition.x, this.prevPosition.y);
+  }
+  
+  update(acc) {
+    this.velocity.x += cos(this.ang) * acc;
+    this.velocity.y += sin(this.ang) * acc;
+    
+    this.prevPosition.x = this.position.x;
+    this.prevPosition.y = this.position.y;
+    
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
+  
+  draw() {
+    const alpha = map(this.velocity.mag(), 0, 3, 0, 255);
+    stroke(255, alpha);
+    line(this.position.x, this.position.y, this.prevPosition.x, this.prevPosition.y);
+  }
+}
+
+function onScreen(x, y) {
+  return x >= 0 && x <= width && y >= 0 && y <= height;  
+}
