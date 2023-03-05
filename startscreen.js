b3ebecb8dcc2d507bee3f04acdc26e8c873d81a2
velklusia <116886960+velklusia@users.noreply.github.com>
2023-03-06 00:45:47 +0100

    draw () function

--- startscreen.js
+++ startscreen.js
@@ -11,3 +11,18 @@ function setup() {
   }
 }
 
+function draw() {
+  background(0, 50);
+  
+  const acc = map(mouseX, 0, width, 0.25, 0.2);
+  
+  asteroids = asteroids.filter(asteroid => {
+    asteroid.draw();
+    asteroid.update(acc);
+    return asteroid.isActive();
+  });
+  
+  while(asteroids.length < numberOfAsteroids) {
+    asteroids.push(new Asteroid(random(width), random(height)));
+  }
+}
