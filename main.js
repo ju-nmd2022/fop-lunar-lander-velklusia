dac1de2d95931c4a3df5499013df8534af3870f2
velklusia <116886960+velklusia@users.noreply.github.com>
2023-03-06 00:52:10 +0100

    Setup

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+function setup() {
+  createCanvas(700, 600);
+  for (let i = 0; i < numberOfStars; i++) {
+    stars.push(new star());
+  }
+}
 
+const numberOfStars = 250;
+const distance = 35;
+let stars = [];
