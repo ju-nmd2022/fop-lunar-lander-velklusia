af14d12efba9d84cfd13a3665bf90c18851f0e02
velklusia <116886960+velklusia@users.noreply.github.com>
2023-03-06 00:45:08 +0100

    Setup for an animation

--- startscreen.js
+++ startscreen.js
@@ -1,1 +1,13 @@
+const numberOfAsteroids = 700;
+let asteroids = [];
+
+function setup() {
+  createCanvas(600, 600);
+  stroke(255);
+  strokeWeight(1);
+  
+  for(let i = 0; i < numberOfAsteroids; i ++) {
+    asteroids.push(new Asteroid(random(width), random(height)));
+  }
+}
 
