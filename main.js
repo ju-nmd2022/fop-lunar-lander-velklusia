03360c4f89283ebabebe2c5625b1bd84224c8d9a
velklusia <116886960+velklusia@users.noreply.github.com>
2023-03-06 19:42:04 +0100

    testing animations

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
+const numberOfAsteroids = 700;
+let asteroids = [];
+
 function setup() {
   createCanvas(700, 600);
   for (let i = 0; i < numberOfStars; i++) {
     stars.push(new star());
   }
+  for(let i = 0; i < numberOfAsteroids; i ++) {
+    asteroids.push(new Asteroid(random(width), random(height)));
+  }
 }
 
+//Main background animation
 const numberOfStars = 250;
 const distance = 35;
 let stars = [];
@@ -45,6 +52,61 @@ drawLines(stars) {
     }
   }
 }
+}
+
+//Mechanics and idea behind was inspired by CodingTrain 10 minutes Starfield Challenge
+// https://www.youtube.com/watch?v=17WoOqgXsRM
+
+function animation() {
+  background(0, 50);
+  
+  const acc = map(mouseX, 0, width, 0.25, 0.2);
+  
+  asteroids = asteroids.filter(asteroid => {
+    asteroid.draw();
+    asteroid.update(acc);
+    return asteroid.isActive();
+  });
+  
+  while(asteroids.length < numberOfAsteroids) {
+    asteroids.push(new Asteroid(random(width), random(height)));
+  }
+}
+
+class Asteroid {
+  constructor(x, y) {
+    this.position = createVector(x, y);
+    this.prevPosition = createVector(x, y);
+    
+    this.velocity = createVector(0, 0);
+    
+    this.ang = atan2(y - (height/2), x - (width/5));
+  }
+  
+  isActive() {
+    return onScreen(this.prevPosition.x, this.prevPosition.y);
+  }
+  
+  update(acc) {
+    this.velocity.x += cos(this.ang) * acc;
+    this.velocity.y += sin(this.ang) * acc;
+    
+    this.prevPosition.x = this.position.x;
+    this.prevPosition.y = this.position.y;
+    
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
+  
+  draw() {
+    const alpha = map(this.velocity.mag(), 0, 3, 0, 255);
+    stroke(255, alpha);
+    line(this.position.x, this.position.y, this.prevPosition.x, this.prevPosition.y);
+  }
+}
+
+function onScreen(x, y) {
+  return x >= 0 && x <= width && y >= 0 && y <= height;  
 }
 
 function rocket() {
@@ -130,7 +192,7 @@ function draw() {
     star.draw();
     star.drawLines(stars);
   }
-  welcomingScreen();
+  winningScreen();
   moon();
   rocket();
 }
@@ -142,7 +204,7 @@ function welcomingScreen(x, y) {
   textSize(20);
   textFont("Courier New");
   text("Press space to initiate", 50, 70);
-  text("Press space to initiate the launching protocol...", 50, 70);
+  text("Press space to initiate the launching sequence...", 50, 70);
   textSize(17);
   textFont("Courier New");
   text("Lunar Lander Game by vel klusia", 50, 100);
@@ -171,4 +233,5 @@ function winningScreen(x, y) {
   textSize(17);
   textFont("Courier New");
   text("The mission ended up as a success.", 50, 100);
+  animation();
 }
